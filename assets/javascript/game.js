856e032626cefe47847088f4728df0158a216b8b
Isabel <iarcones@gmail.com>
2019-10-22 17:33:13 -0700

    solved issues with city multi word, example New York

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,145 +1,190 @@
+var cities = [
+  "New York",
+  "Los Angeles",
+  "Chicago",
+  "Houston",
+  "Phoenix",
+  "Philadelphia",
+  "San Antonio",
+  "San Diego",
+  "Dallas",
+  "San Jose",
+  "Austin",
+  "Jacksonville",
+  "San Francisco",
+  "Columbus",
+  "Fort Worth",
+  "Indianapolis",
+  "Charlotte",
+  "Seattle",
+  "Denver",
+  "Washington",
+  "Boston",
+  "El Paso",
+  "Detroit",
+  "Nashville",
+  "Memphis",
+  "Portland",
+  "Oklahoma City",
+  "Las Vegas",
+  "Louisville",
+  "Baltimore",
+  "Milwaukee",
+  "Albuquerque",
+  "Tucson",
+  "Fresno",
+  "Sacramento",
+  "Mesa",
+  "Kansas City",
+  "Atlanta",
+  "Long Beach",
+  "Omaha",
+  "Raleigh",
+  "Colorado Springs",
+  "Miami",
+  "Virginia Beach",
+  "Oakland",
+  "Minneapolis",
+  "Tulsa",
+  "Arlington",
+  "New Orleans",
+  "Wichita"
+];
 
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
-
-var cities = ["NewYork", "LosAngeles", "Chicago", "Houston" , "Phoenix", "Philadelphia", "SanAntonio", "SanDiego", "Dallas", "SanJose", "Austin", "Jacksonville", "SanFrancisco", "Columbus", "FortWorth", "Indianapolis", "Charlotte", "Seattle", "Denver", "Washington", "Boston", "ElPaso", "Detroit", "Nashville", "Memphis", "Portland", "OklahomaCity", "LasVegas", "Louisville", "Baltimore", "Milwaukee", "Albuquerque", "Tucson", "Fresno", "Sacramento", "Mesa", "KansasCity", "Atlanta", "LongBeach", "Omaha", "Raleigh", "ColoradoSprings", "Miami", "VirginiaBeach", "Oakland", "Minneapolis", "Tulsa", "Arlington", "NewOrleans", "Wichita"];
-
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  
-
-var city = cities[Math.floor(Math.random() * cities.length)];
-
-console.log(city);
-
-var lCity = city.toLowerCase();
-var arraylCity = lCity.split('');
-
-
-var arrayCity = city.split(" ");
-var numberWords = arrayCity.length;
-
-var attempts = 0;
-var lettersFail = [];
-var gameEnded = false;
-
-
-var img = "./assets/images/" + city + ".jpg";
-document.getElementById("idimg").src = img;
-document.getElementById("idimg").innerHTML = img;
-
-var hangmanImgPath = "./assets/images/Hangman-0.png";
-document.getElementById("idhangman").src = hangmanImgPath;
-document.getElementById("idhangman").innerHTML = hangmanImgPath;
-
-document.querySelector("#idFail").innerHTML = "Attempts failed: " + attempts;
-document.querySelector("#idlettersFail").innerHTML = "Misses:";
+var city;
+var lCity;
+var arraylCity;
+var attempts;
+var lettersFail;
+var word;
+var wins = 0;
+var losses = 0;
 
 function reset() {
-//  reset word array
-attempts = 0
+  city = cities[Math.floor(Math.random() * cities.length)];
+  console.log(city);
+
+  lCity = city.toLowerCase();
+  attempts = 6;
+  lettersFail = "";
+  word = "";
+
+  var img = "./assets/images/" + city.split(" ").join("") + ".jpg";
+  console.log(img);
+  document.getElementById("idimg").src = img;
+
+  var hangmanImgPath = "./assets/images/Hangman-0.png";
+  document.getElementById("idhangman").src = hangmanImgPath;
+  document.getElementById("idhangman").innerHTML = hangmanImgPath;
+
+  document.querySelector("#idFail").innerHTML = "Left: " + attempts;
+  document.querySelector("#idlettersFail").innerHTML = "Wrong Guesses:";
+
+  for (var i = 0; i < lCity.length; i++) {
+    if (lCity[i] === " ") {
+      word += " ";
+    } else {
+      word += "-";
+    }
+  }
+
+  document.querySelector("#idtheword").innerHTML = word;
+  document.querySelector("#idFinal").innerHTML = "";
+  document.querySelector("#iderrors").innerHTML = "";
 }
 
+function updateFail() {
+  document.querySelector("#idFail").innerHTML = "left: " + attempts;
+  document.querySelector("#idlettersFail").innerHTML =
+    "Wrong Guesses: " + lettersFail;
+  var hangmanImgPath = "./assets/images/Hangman-" + attempts + ".png";
+  document.getElementById("idhangman").src = hangmanImgPath;
+  document.getElementById("idhangman").innerHTML = hangmanImgPath;
+}
 
-    var nlfirstWord = city.length;
-    document.querySelector("#idfirstWord").innerHTML = "Number of letters: " + nlfirstWord;
-    
-    var word = [];
-  
-    for (var i = 0; i < nlfirstWord; i++) { 
-       
-        word.push("-");
-        }
+function checkletters(userInput) {
+  console.log("-->", userInput, lCity);
 
-    document.querySelector("#idtheword").innerHTML = "City: " + word;
-  
-
-// FUNCTIONS
-//==============================================================
-
-    // Function that updates fails
-    function updateFail() {
-        document.querySelector("#idFail").innerHTML = "Attempts failed: " + attempts;
-        document.querySelector("#idlettersFail").innerHTML = "Misses: " + lettersFail;
-        var hangmanImgPath = "./assets/images/Hangman-" + attempts + ".png"
-        document.getElementById("idhangman").src = hangmanImgPath;
-        document.getElementById("idhangman").innerHTML = hangmanImgPath;
-    }  
-
-    function checkletters(userInput) {
- 
-        var lettersFounded = "";
-        var letterPosition = [];
-        var nlettersFounded = 0;
-     
-        for (var i = 0; i < lCity.length; i++) {
-            if (lCity[i] === userInput) {
-       
-                lettersFounded =  lettersFounded + userInput;
-                letterPosition.push(i);
-            }
-        }
-     
-        nlettersFounded = lettersFounded.length;
-    
-
-        if (nlettersFounded ===  0 ) {
-            attempts++
-            lettersFail.push(userInput);
-            updateFail();
-        }
-        else {
-        
-            for (var i = 0; i < lCity.length; i++) {
-                var n = (letterPosition.indexOf(i));
-
-                if (n !== -1) {
-                    word[i] = userInput;
-                }
-            }
-     
-            document.querySelector("#idtheword").innerHTML = "City: " + word;
-        }
+  if (lCity.includes(userInput)) {
+    var wordTemp = "";
+    for (var i = 0; i < lCity.length; i++) {
+      if (lCity[i] === userInput) {
+        wordTemp += userInput;
+      } else {
+        wordTemp += word[i];
+      }
     }
-// MAIN PROCESS
-//==============================================================
+    word = wordTemp;
+    console.log("-->", wordTemp);
+    if (!word.includes("-")) {
+      document.querySelector("#idFinal").innerHTML =
+        "Congrats you won!. Game Over!. Try again";
+      wins++;
 
-
-    // When the user presses a key, it will run the following function...
-    document.onkeyup = function(event) {
-        var userInput = event.key.toLowerCase();
-
-       if (alphabet.indexOf(userInput) === -1){
-
-        document.querySelector("#iderrors").innerHTML = "It's not a letter";
-       }
-        else {  
-            if (gameEnded){ 
-                document.querySelector("#endgame").innerHTML = "GAME HAS ENDED, IF YOU WANT TO PLAY RELOAD THE PAGE";
-            }
-            else {      
-                var rletterFail = (lettersFail.indexOf(userInput));
-                var rletterNofail = (word.indexOf(userInput));
-
-                document.querySelector("#iderrors").innerHTML = " ";
-
-                if (rletterFail === -1 && rletterNofail === -1) {
-                    checkletters(userInput);
-
-                    if (word.toString() === arraylCity.toString()) {
-                        document.querySelector("#idFinal").innerHTML = "Congrats you won!. <br> Game Over!";
-                        gameEnded = true;
-                        //reset()
-                    }
-
-                    if (attempts === 6) {
-                        document.querySelector("#idFinal").innerHTML = ":-( You lost. <br> Game Over!";
-                        gameEnded = true;
-                        //reset()
-                    }
-                
-                }
-                else {
-                    document.querySelector("#iderrors").innerHTML = "Key repeated";
-                }
-            }
-        }
+      document.querySelector("#wins").innerHTML = wins;
+      setTimeout(reset, 4000);
     }
+  } else {
+    attempts--;
+    lettersFail += userInput;
+    updateFail();
+  }
+
+  if (attempts === 0) {
+    document.querySelector("#idFinal").innerHTML =
+      ":-( You lost. Game Over!. Try again";
+    losses++;
+
+    document.querySelector("#losses").textContent = losses;
+    setTimeout(reset, 4000);
+  }
+
+  document.querySelector("#idtheword").innerHTML = word;
+}
+
+document.onkeyup = function(event) {
+  console.log(event.key);
+  var userInput = event.key.toLowerCase();
+  document.querySelector("#iderrors").innerHTML = "";
+
+  if (alphabet.indexOf(userInput) === -1) {
+    document.querySelector("#iderrors").innerHTML = "It's not a letter";
+  } else {
+    if (!word.includes(userInput) && !lettersFail.includes(userInput)) {
+      checkletters(userInput);
+    } else {
+      document.querySelector("#iderrors").innerHTML = "Key repeated";
+    }
+  }
+};
+
+reset();
